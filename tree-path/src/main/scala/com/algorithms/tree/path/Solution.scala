92c9f9001512b3f3753d5e3af5981f4f7289aa9b
drazens <drazen.skobic@mail.com>
2017-03-25 18:10:18 +0000

    Tree patrh - Implementation

--- tree-path/src/main/scala/com/algorithms/tree/path/Solution.scala
+++ tree-path/src/main/scala/com/algorithms/tree/path/Solution.scala
@@ -1,7 +1,61 @@
 package com.algorithms.tree.path
 
+class Tree(var x: Int, var l: Tree, var r: Tree)
+
+sealed trait MyTree
+case class Node(value: Int, left: MyTree, right: MyTree) extends MyTree
+case object Leaf extends MyTree
+
 object Solution {
 
-  def solution(): Unit = ???
+  import scala.collection.mutable.Set
+
+  def solution(t: Tree): Int =
+    mostDistinctPath(toMyTree(t))
+
+  private def mostDistinctPath(t: MyTree): Int = {
+    val paths = distinctPaths(t)
+    val max = paths.reduce { (s, acc) =>
+      if (s.size > acc.size)
+        s
+      else
+        acc
+    }
+
+    max.size
+  }
+
+  private def distinctPaths(t: MyTree): List[Set[Int]] = t match {
+    case Leaf =>
+      List(Set[Int]())
+    case Node(v, l, r) =>
+      val leftPaths = distinctPaths(l)
+      val rightPaths = distinctPaths(r)
+
+      leftPaths map { _ += v }
+      rightPaths map { _ += v }
+
+      leftPaths ::: rightPaths
+  }
+
+  private def toMyTree(t: Tree): MyTree = {
+    if (t == null)
+      Leaf
+    else {
+      val left: MyTree =
+        if (t.l == null)
+          Leaf
+        else
+          toMyTree(t.l)
+
+      val right: MyTree =
+        if (t.r == null)
+          Leaf
+        else
+          toMyTree(t.r)
+
+      Node(t.x, left, right)
+    }
+  }
 
 }
--- tree-path/src/test/scala/com/algorithms/tree/path/SolutionSuite.scala
+++ tree-path/src/test/scala/com/algorithms/tree/path/SolutionSuite.scala
@@ -9,4 +9,28 @@ import Solution.solution
 @RunWith(classOf[JUnitRunner])
 class SolutionSuite extends FunSuite {
 
+  test("Find most distinct path for empty tree") {
+    val a: Tree = null
+
+    val expected = 0
+    val actual = solution(a)
+
+    assert(actual == expected)
+  }
+
+  test("Find most distinct path for non-empty tree") {
+    val g = new Tree(5, null, null)
+    val d = new Tree(4, g, null)
+    val b = new Tree(5, d, null)
+    val e = new Tree(1, null, null)
+    val f = new Tree(6, null, null)
+    val c = new Tree(6, e, f)
+    val a = new Tree(4, b, c)
+
+    val expected = 3
+    val actual = solution(a)
+
+    assert(actual == expected)
+  }
+
 }
